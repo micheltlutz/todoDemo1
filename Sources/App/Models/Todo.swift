c3154fe96600b1f3af5b2835e58f544d4ca368ed
Michel Anderson Lutz Teixeira <michel@MARK5.local>
2018-08-15 22:28:27 -0300

    first commit

--- Sources/App/Models/Todo.swift
+++ Sources/App/Models/Todo.swift
@@ -1,18 +1,22 @@
-import FluentSQLite
 import Vapor
+import FluentPostgreSQL
 
 /// A single entry of a Todo list.
-final class Todo: SQLiteModel {
+final class Todo: PostgreSQLModel {
     /// The unique identifier for this `Todo`.
     var id: Int?
 
     /// A title describing what this `Todo` entails.
     var title: String
+    
+    /// A flag describing if this `Todo` is done.
+    var done: Bool?
 
     /// Creates a new `Todo`.
-    init(id: Int? = nil, title: String) {
+    init(id: Int? = nil, title: String, done: Bool? = false) {
         self.id = id
         self.title = title
+        self.done = false
     }
 }
 
